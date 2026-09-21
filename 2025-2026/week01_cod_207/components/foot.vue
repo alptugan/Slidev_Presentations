e22297a93a7f7d691148a7ea588288c0ceedd2f6
alp tuğan <alptugan@gmail.com>
2026-09-21 15:23:35 +0300

    week 01

---
+++ 2025-2026/week01_cod_207/components/foot.vue
@@ -0,0 +1,8 @@
+<template>
+  <div
+    v-if="$slidev.nav.currentLayout !== 'cover'"
+    class="absolute bottom-0 left-50% right-0 p-2"
+  >
+    {{ $slidev.nav.currentPage }} / {{ $slidev.nav.total }}
+</div>
+</template>
